33315156a75549cab25bb8890da6d9186bd046c7
lusifer9527 <lusifer9527@gmail.com>
2022-04-11 00:26:32 +0800

    'update'

---
+++ bin/r7000/一键本地预览.bat
@@ -0,0 +1,3 @@
+@echo off
+cd D:\dev\hexo\leblog.github.io
+hexo clean&&hexo g&&hexo s
---
+++ bin/r7000/博客源文件一键push-le.bat
@@ -0,0 +1,5 @@
+@echo off
+cd D:\dev\hexo\leblog.github.io
+git add .
+git commit -m 'update'
+git push origin le
